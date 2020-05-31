9e99ceffa551454762fe79c11e85994481e1ec42
Now_Sprouting <15542456716@163.com>
2020-05-31 16:40:55 +0800

    基本框架搭建完成

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 移动端解决点击延迟
+import fastClick from 'fastclick'
+// 移动端样式初始化
+import './assets/styles/reset.css'
+// 2px边框问题解决方案
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   router,
---
+++ src/views/home/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>home</div>
+    <a href="javascrip:;">aaaa</a>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+</style>
